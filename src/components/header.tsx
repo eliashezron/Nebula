6885b7fe9b9ef9d0d8313ee9bb5e8eb1dd38a209
eliashezron <eliashezron23@gmail.com>
2024-02-20 00:19:36 +0300

    making changes to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,17 +6,23 @@ const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="flex justify-between items-center">
-      <div className="text-lg md:text-2xl font-bold">NEBULA</div>
-      <div className="flex items-center md:hidden">
+    <nav className="flex items-center justify-between w-full">
+      <div className="flex items-center justify-between flex-grow">
+        <div className="text-lg md:text-2xl font-bold">NEBULA</div>
+        <a href="https://t.me/+GCR4p8G0ylwxZTQ0" target="_blank" rel="noopener noreferrer" className="hidden md:flex items-center text-black hover:text-blue-500">
+          <span className='py-2 mr-3'>Reach Support via</span>
+          <img src='telegram.svg' alt='Telegram icon' className='w-12 h-12'/>
+        </a>
+      </div>
+      <div className="md:hidden">
         <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
           {isMenuOpen ? <XIcon className="h-6 w-6" /> : <MenuIcon className="h-6 w-6" />}
         </button>
       </div>
-      <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 absolute md:relative top-16 left-0 w-full md:w-auto bg-white md:bg-transparent z-20 p-4 md:p-0`}>
-        <a href="https://t.me/+GCR4p8G0ylwxZTQ0" target="_blank" rel="noopener noreferrer" className="text-black-300 hover:text-white flex items-center justify-center">
+      <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:hidden absolute top-16 left-0 w-full bg-white z-20 p-4`}>
+        <a href="https://t.me/+GCR4p8G0ylwxZTQ0" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center w-full">
           <span className='py-2 mr-3'>Reach Support via</span>
-          <img src='telegram.svg' alt='scan icon' className='w-12 h-12'/>
+          <img src='telegram.svg' alt='Telegram icon' className='w-12 h-12'/>
         </a>
       </div>
     </nav>
