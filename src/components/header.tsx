dfc95bf73f4b071e5a51f684d78528fb9272c27f
eliashezron <eliashezron23@gmail.com>
2024-02-21 09:40:52 +0300

    ui changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
         <div className="text-lg md:text-2xl font-bold">NEBULA</div>
         <a href="https://t.me/+GCR4p8G0ylwxZTQ0" target="_blank" rel="noopener noreferrer" className="hidden md:flex items-center text-black hover:text-blue-500">
           <span className='py-2 mr-3'>Reach Support via</span>
-          <img src='telegram.svg' alt='Telegram' className='w-12 h-12'/>
+          <img src='/telegram.svg' alt='Telegram' className='w-12 h-12'/>
         </a>
       </div>
       <div className="md:hidden">
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
       <div className={`${isMenuOpen ? 'flex' : 'hidden'} md:hidden absolute top-16 left-0 w-full bg-white z-20 p-4`}>
         <a href="https://t.me/+GCR4p8G0ylwxZTQ0" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center w-full">
           <span className='py-2 mr-3'>Reach Support via</span>
-          <img src='telegram.svg' alt='Telegram icon' className='w-12 h-12'/>
+          <img src='/telegram.svg' alt='Telegram icon' className='w-12 h-12'/>
         </a>
       </div>
     </nav>
