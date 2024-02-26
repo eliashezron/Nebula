b5321fcc02ec25ffa5130f04fbfd2c48b8516add
eliashezron <eliashezron23@gmail.com>
2024-02-26 09:02:05 +0300

    ui changes

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -7,21 +7,21 @@ interface FilterBarProps {
 
 const FilterBar: React.FC<FilterBarProps> = ({ onFilterChange }) => {
   return (
-    <div className="flex justify-center space-x-4 my-4">
+    <div className="flex justify-center space-x-4 my-4 ">
       <button
-        className="border px-4 py-2 rounded shadow flex"
+        className="border px-4 py-2 bg-white rounded shadow flex"
         onClick={() => onFilterChange('mostScanned')}
       >
         <img src='scan.svg' alt='scan icon' className='w-5 h-5 mr-2'/><span>Tokens: Most Scanned</span>
       </button>
       <button
-        className="border px-4 py-2 rounded shadow flex"
+        className="border px-4 py-2  bg-white rounded shadow flex"
         onClick={() => onFilterChange('recentThreats')}
       >
        <img src='recent.svg' alt='recent icon' className='w-5 h-5 mr-2'/><span>Tokens: Recent Threats</span>
       </button>
       <button
-        className="border px-4 py-2 rounded shadow flex"
+        className="border px-4 py-2 bg-white  rounded shadow flex"
         onClick={() => onFilterChange('marketCap')}
       >
         <img src='marketgraph.svg' alt='market grap icon' className='w-5 h-5 mr-2'/><span>Tokens: marketCap</span>
